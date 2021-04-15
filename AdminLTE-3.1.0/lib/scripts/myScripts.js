0e366b6cb0918fe0d92098f937bade2994996dae
lozanosimon023 <lozanosimon.023@gmail.com>
2021-04-15 18:52:37 -0500

    SE FORMATEA EL CODIGO

--- AdminLTE-3.1.0/lib/scripts/myScripts.js
+++ AdminLTE-3.1.0/lib/scripts/myScripts.js
@@ -1,12 +1,11 @@
-function cargar(url ){
-    if(menu = "")
-    var url="tabla.php"
-    $.ajax({   
-        type: "POST",
-        url:url,
-        data:{},
-        success: function(datos){       
-            $('#tabla').html(datos);
-        }
-    });
+function cargar(url) {
+  if ((menu = "")) var url = "tabla.php";
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: {},
+    success: function (datos) {
+      $("#tabla").html(datos);
+    },
+  });
 }

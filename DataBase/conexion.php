578902b93a44f0a48008638f11e9bfa45356b03d
lozanosimon023 <lozanosimon.023@gmail.com>
2021-08-06 12:17:04 -0500

    Nueva Conexion.php a modelo

--- DataBase/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $user="root";
-    $password="";
-    $server="localhost";
-    $database="db_veterinaria";
-    $conexion = mysqli_connect($server, $user, $password, $database);
-    mysqli_set_charset($conexion, "utf8");
-?>
